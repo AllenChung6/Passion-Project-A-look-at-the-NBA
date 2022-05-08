8e7cc9f74988bfe6611e591446205b3bab46b1da
Allen Chung <allen.chung06@gmail.com>
2022-05-08 03:03:52 -0400

    Added line chart.

--- NBA_Analysis_Frontend.py
+++ NBA_Analysis_Frontend.py
@@ -7,8 +7,9 @@ def load_data(nrows):
     data = pd.read_csv('output_files/Player_data.csv')
     return data
 
-header = st.beta_container()
-dataset = st.beta_container()
+header = st.container()
+dataset = st.container()
+visuals = st.container()
 
 with header:
     st.title('NBA Player Compensation Analysis') 
@@ -16,7 +17,19 @@ with header:
     fairly paid, or underpaid vs their playing performance.')
 
 with dataset:   
-    st.write("NBA Clean Dataset")
-    st.write("The datasets are from Basketballreference.com")
+    st.write("See how I created the NBA Dataset by cleaning and transforming different datasets here: \
+         [link](https://share.streamlit.io/mesmith027/streamlit_webapps/main/MC_pi/streamlit_app.py)")
+    st.write('NBA Cleaned Dataset')
+    st.markdown('The dataset used is from Basketballreference.com')
+    data = pd.read_csv('output_files/Player_data.csv')
+    data
+
+with visuals:
+    st.write('Line Chart:')
+    chart_data = pd.DataFrame(
+        data=data,
+        columns=['Performance','Age',]
+    )
+    st.line_chart(chart_data)
     
-data = pd.read_csv('output_files/Player_data.csv')
+    
