66a2ba1f6383a40d34754f10fd6673c2a6ebdb2d
Allen Chung <allen.chung06@gmail.com>
2022-05-08 00:52:38 -0400

    Updated descriptions for header and dataset.

--- NBA_Analysis_Frontend.py
+++ NBA_Analysis_Frontend.py
@@ -1,13 +1,22 @@
 import pandas as pd
 import streamlit as st
-
-st.title('NBA Player Compensation Analysis')   
+   
 
 @st.cache
 def load_data(nrows):
     data = pd.read_csv('output_files/Player_data.csv')
     return data
 
-st.write("NBA DATASET")
+header = st.beta_container()
+dataset = st.beta_container()
+
+with header:
+    st.title('NBA Player Compensation Analysis') 
+    st.markdown('My goal in this project is to analyze data of the top 50 highest paid NBA players and try to determine if they are overpaid, \
+    fairly paid, or underpaid vs their playing performance.')
+
+with dataset:   
+    st.write("NBA Clean Dataset")
+    st.write("The datasets are from Basketballreference.com")
+    
 data = pd.read_csv('output_files/Player_data.csv')
-st.write(data)
