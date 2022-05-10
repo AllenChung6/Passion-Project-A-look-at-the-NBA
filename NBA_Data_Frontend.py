c4e44a0d4aa911fd9417527b61ad7152cbfbbbb7
Allen Chung <allen.chung06@gmail.com>
2022-05-10 00:24:41 -0400

    Added highlighed color to Analysis column.

--- NBA_Data_Frontend.py
+++ NBA_Data_Frontend.py
@@ -11,6 +11,7 @@ header = st.container()
 dataset = st.container()
 visuals = st.container()
 features = st.container()
+adhoc = st.container()
 
 
 with header:
@@ -33,6 +34,7 @@ def load_data(year):
         data = html[0]
         raw=data.drop(data[data.Age == 'Age'].index)
         raw = raw.fillna(0)
+        raw = raw.rename(columns={'Tm': 'Team'})
         playerstats = raw.drop(['Rk'], axis=1)
         return playerstats
 playerstats = load_data(selected_year)
@@ -49,43 +51,52 @@ def load_data(nrows):
 
 with features:
     # Team Selection. Sort by unique team name and add elements to selected_team.
-    sorted_by_unique_team = sorted(playerstats.Tm.unique())
-    selected_team = st.sidebar.selectbox('Tm', list((x for x in sorted_by_unique_team)))
+    sorted_by_unique_team = sorted(playerstats.Team.unique())
+    selected_team = st.sidebar.selectbox('Team', list((x for x in sorted_by_unique_team)))
 
     # Position Selection
     uniq_pos = ['PG','SG','SF','PF','C']
     sel_pos = st.sidebar.multiselect('Position', uniq_pos)
 
 
+
 with dataset:
     # Filtering Data
-    df_selected_team = playerstats[(playerstats.Tm == (selected_team)) & (playerstats.Pos.isin(sel_pos))]
+    df_selected_team = playerstats[(playerstats.Team == (selected_team)) & (playerstats.Pos.isin(sel_pos))]
 
     st.write('Display Player Stats below:')
     try:
     # To avoid StreamlitAPIException: ("Eexpected bytes, got a 'int' object", 'Conversion failed for column....'), converted to str
-        team_df = df_selected_team.astype(str)
-        st.dataframe(team_df)
+        player_df = df_selected_team.astype(str)
+        st.dataframe(player_df)
     except Exception as e:
         st.error('There was an error. Please reload app')
     else:
         st.success('Dataset queried. Data Dimension: ' + str(df_selected_team.shape[0]) + ' rows and ' + str(df_selected_team.shape[1]) + ' columns.')
 
-# Download data as csv file
-csv = convert_df(team_df)
-st.download_button(
-label="Download data as CSV",
-data=csv,
-file_name= f'{selected_team}_Player_data.csv',
-mime='text/csv',
-)
 
-# Import CSV file
-st.header('Ad-hoc Analysis: Player Salary vs Performance')
-st.write('NBA Cleaned Dataset')
-st.markdown('The dataset used is from Basketballreference.com')
-data = pd.read_csv('output_files/Player_data.csv')
-data
+    # Download data as csv file
+    csv = convert_df(player_df)
+    st.download_button(
+    label="Download data as CSV",
+    data=csv,
+    file_name= f'{selected_team}_Player_data.csv',
+    mime='text/csv',
+    )
+
+with adhoc:
+    # Import CSV file
+    st.header('Ad-hoc Analysis: Player Salary vs Performance')
+    st.write('NBA Cleaned Dataset')
+    st.markdown('The dataset used is from Basketballreference.com')
+    data = pd.read_csv('output_files/Player_data.csv')
+    # data
+
+    def color(val):
+        color = 'red' if val=='Overpaid' else 'orange' if val=='Fairly Paid' else 'green'
+        return f'background-color: {color}'
+
+    st.dataframe(data.style.applymap(color, subset=['Analysis']))
 
 # Download data as xls file
 # with open(output_file, "rb") as fp:
